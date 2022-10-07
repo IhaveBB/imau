b0be10f7b8b51312f407530c27129147a5b36953
IhaveBB <115210411+IhaveBB@users.noreply.github.com>
2022-10-07 14:47:14 +0800

    Create 判断三个数中最大值.c

--- 判断三个数中最大值.c
+++ 判断三个数中最大值.c
@@ -1,1 +1,21 @@
-
+//有三个整数a b c,由键盘输入，输出其中的最大的数
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int x;
+    int m;
+    scanf("%d%d%d",&a, &b, &c);
+    if(a>b)
+        x = a;
+    else
+        x = b;
+    if(x>c)
+        m = x;
+    else
+        m = c;
+    printf("%d",m);
+    return 0;
+}
