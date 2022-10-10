b913b486999016e9cff43b3d67b44307382aae6f
IhaveBB <115210411+IhaveBB@users.noreply.github.com>
2022-10-10 21:37:58 +0800

    Update 10.10作业.c

--- 10.10作业.c
+++ 10.10作业.c
@@ -550,4 +550,38 @@ int main()
 		printf("%d数能被7整除\n", x);
 	return 0;
 }
-
+#define _CRT_SECURE_NO_WARNINGS
+//（17）    编程设计一个简单的计算器程序，要求根据用户从键盘输入的表达式计算，并使用if语句实现。
+//
+//操作数1  运算符op  操作数2  计算表达式的值。指定的运算符为加(+)、减(-)、乘(*)、除(/ )。
+#include<stdio.h>
+int main()
+{
+	int x, y;
+	char op;
+	float result;
+	printf("please input number operator number");
+	scanf("%d%c%d", &x, &op, &y);
+	if (op == '+')
+	{
+		result = x + y;
+		printf("result=%0.2f", result);
+	}
+	else if (op == '-')
+	{
+		result = x - y;
+		printf("result=%0.2f", result);
+	}
+	else if (op == '*')
+	{
+		result = x * y;
+		printf("result=%0.2f", result);
+	}
+	else if (op == '/')
+	{
+		result = x / y;
+		printf("result=%0.2f", result);
+	}
+	return 0;
+}
+	
