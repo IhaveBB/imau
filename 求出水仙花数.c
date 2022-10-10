37da192363bffb16ef23c3fccdd321dd9d77ae00
IhaveBB <115210411+IhaveBB@users.noreply.github.com>
2022-10-10 21:11:15 +0800

    Update 求出水仙花数.c

--- 求出水仙花数.c
+++ 求出水仙花数.c
@@ -1,13 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+//（15）    输入一个三位数，若此数是水仙花数输出“Y”，否则输出“N”， 若输入值不是三位数输出“data error”。
+//
+//（提示：水仙花数是一个三位数，组成这个三位数的三个数字的立方和与这个三位数相等。如：153 = 13 + 53 + 33。
+//判断是否是水仙花数需把构成三位数的三个数字分离出来并存入变量。）
 #include<stdio.h>
-int main() {
-    int a, ge, shi, bai;
-    for (a = 100; a <= 1000; a++) {
-        ge = a / 1 % 10;
-        shi = a / 10 % 10;
-        bai = a / 100 % 10;
-        if (a == ge * ge * ge + shi * shi * shi + bai * bai * bai) {
-            printf("%d\n", a);
-        }
-    }
-    return 0;
+int main()
+{
+	int x,a,b,c;
+	printf("please input a number");
+	scanf("%d", &x);
+	if (x < 1000)
+	{
+		a = x / 100;
+		b = x % 100 / 10;
+		c = x % 10 / 1;
+		if (x == a*a*a+b*b*b+c*c*c)
+		{
+			printf("%d是水仙花数", x);
+		}
+		else
+		{
+			printf("%d不是水仙花数", x);
+		}
+	}
+	else
+	{
+		printf("error");
+	}
+	return 0;
 }
