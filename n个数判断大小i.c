9310f8bc8428c353f7885b34148cab616bc70417
IhaveBB <115210411+IhaveBB@users.noreply.github.com>
2022-10-10 12:09:46 +0800

    Create n个数判断大小i.c

---
+++ n个数判断大小i.c
@@ -0,0 +1,19 @@
+
+//#include<stdio.h>
+//int main()
+//{
+//	int n, i;
+//	float x, max;  //因为没有限定是否为整形数字，所以用float
+//	printf("请输入数字的个数：");
+//	scanf("%d", &n);
+//	printf("请输入%d个数字\n", n);
+//	max = -1e30;//-1e30仅仅是为了取一个尽量小的值
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%f", &x);
+//		if (x > max)
+//			max = x;
+//	}
+//	printf("最大值为：%0.3f\n", max);  //可是设置浮点数保留几位小数
+//	return 0;
+//}
